40b6fa53b7210581b9ecc1b7f64b0fd55431e92e
YamgoKES <eunseokid@naver.com>
2025-07-08 11:54:45 +0900

    javascript and css 수정

--- frontend/src/components/EmotionSummary.jsx
+++ frontend/src/components/EmotionSummary.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-function EmotionSummary(){
+function EmotionSummary({promptText}){
+    if (!promptText) return <div className="emotion-summary"><p>Recommendation and Emotionsummary</p></div>;
+
     return (
         <div className="emotion-summary">
-            <p>오늘은 기분이 좀 우울해 보이네요,</p>
-            <p>20대 여성인 당신에게 위로가 될 수 있는 아이템들을 추천해드릴게요</p>
+            {promptText.split('\n').map((line,idx)=>(
+                <p key={idx}>{line}</p>
+            ))}
         </div>
     );
 }
--- frontend/src/components/RecommendationList.jsx
+++ frontend/src/components/RecommendationList.jsx
@@ -1,24 +1,43 @@
 import React from 'react';
+import best from '../assets/best.png';
+import best2 from '../assets/best2.png';
+import best3 from '../assets/best3.png';
+import best4 from '../assets/best4.png';
+import best5 from '../assets/best5.png';
 
-function Recommendation(){
-  
-  return(
-    <> 
-      <div className="recommand">
+const iconList = [ best, best2, best3, best4, best5]
+
+function Recommendation({ items }) {
+  return (
+    <div className="recommand">
       <h3>You might like</h3>
 
       <div className="items">
-        {[...Array(5)].map((_, index) => (
-          <div className="item-card" key={index}>
-            <img src="이미지경로" alt="non-image" />
-            <p>상품명</p>
-            <span>shop now</span>
-          </div>
-        ))}
+        {items && items.length > 0 ? (
+          items.map((item, idx) => (
+            <div className="item-card" key={idx}>
+              <img src={iconList[idx % iconList.length]} alt="icon" className="recommend-icon" />
+              <p title={item.title} className="product-title">{item.title}</p>
+              <a href={item.link} target="_blank" rel="noopener noreferrer" className="shop-now-link">
+                Shop Now
+              </a>
+            </div>
+          ))
+        ) : (
+          <p
+              style={{
+                marginLeft: '30px',
+                color: '#333',
+                fontWeight: 'bold',     // 글자 굵게
+                textAlign: 'center',    // 텍스트 가운데 정렬
+              }}
+          >
+              분석을 완료하면 여기에 추천 항목이 나타납니다.
+          </p>
+        )}
       </div>
     </div>
-  </> 
-    );
+  );
 }
 
 export default Recommendation;
